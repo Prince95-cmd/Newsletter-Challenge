7f7bb5a7ecd824475d2688c97d820d33a12d920a
Prince95-cmd <obiekezieprincec@gmail.com>
2024-04-13 13:19:53 +0100

    first commit

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -1,31 +1,28 @@
 window.addEventListener("DOMContentLoaded", () => {
+  //Calling all the elements
   const inputEmail = document.getElementById("email");
   const signUp = document.getElementById("send");
   const errorMessage = document.getElementById("error");
   const displayEmail = document.getElementById("reveal");
 
-  
-  signUp.addEventListener("click", () => {
-    if(inputEmail.value.trim() !== "") {
-        window.location.assign("./Success/index.html");
-    
-        
+  // Calling the button function
+  signUp.addEventListener("click", (e) => {
+    e.preventDefault();
 
-        
-    
+    //Conditional statements  
+    if( inputEmail.validity.valid){
+        window.location.assign("../Success/index.html");
     } else{
       inputEmail.style.backgroundColor = "rgba(255, 98, 87, 0.2)";
       inputEmail.style.border = "2px solid rgb(255, 98, 87)";
       errorMessage.style.display = "inline";
-      
-      
-      
-      inputEmail.addEventListener("mouseout", () => {
-        inputEmail.style.backgroundColor = "";
-        inputEmail.style.border = "";
-        errorMessage.style.display = "none";
-      });
-    
     }
+  });
+  
+  // mouse over effect on iput field
+  inputEmail.addEventListener("mouseout", () => {
+    inputEmail.style.backgroundColor = "transparent";
+    inputEmail.style.border = "";
+    errorMessage.style.display = "none";
   });
 });
